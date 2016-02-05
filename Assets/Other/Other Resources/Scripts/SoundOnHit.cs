4e6350e0160ede809204a55f8f58237a8b178713
Richie Bachtiar <richiebachtiar@gmail.com>
2016-02-05 22:14:20 +0700

    New Bonang Prefab
    di Asset/Other

---
+++ Assets/Other/Other Resources/Scripts/SoundOnHit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundOnHit : MonoBehaviour {
+
+	/*Note :
+	 * Script ini buat dipasang di object yang bakal ngeluarin bunyi
+	 * Pre-req : Object yang dipasang harus punya component "AudioSource"
+	 */
+
+	[Header("Sound")] //Subject To Change : Bisa jadi nanti suaranya di pasang di array bukan di gameObjectnya langsung
+	[SerializeField] AudioClip audioToPlay; //Suara yang dikeluarkan
+	AudioSource sourceMgr; //AudioSource Component untuk handle suara
+
+	[Header("Triggered By")]
+	[SerializeField] GameObject obj; //Object yang ngebuat suara di object ini keluar, Syarat : Object tersebut punya collider + RigidBody
+
+	void Start () {
+		sourceMgr = this.GetComponent<AudioSource> ();
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.name.CompareTo (obj.name) == 0) {
+			sourceMgr.PlayOneShot(audioToPlay);
+		}
+	}
+	
+
+}
