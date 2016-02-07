76fd048efad9816b10bcf4b177acc1dac4965d56
rhne <eindivinereinherjar@gmail.com>
2016-02-07 17:04:17 +0700

    Ada 3 script yg aku buat 1. ToolPemukul.cs => buat tool sekarang 2. GestureHands.cs + PemukulLeapInteraction => codingan orang copas buat coba2 doang Jadi sekarang... Di bagian debug nya, walaupun gak ada tangan/tool, tetap muncul "ADA". Padahal harusnya gak ada

---
+++ Assets/Other/Scripts/GestureHands.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class GestureHands : MonoBehaviour {
+	Controller controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = new Controller();
+		controller.EnableGesture (Gesture.GestureType.TYPESWIPE);
+		controller.Config.SetFloat ("Gesture.Swipe.MinLength", 200.0f);
+		controller.Config.SetFloat ("Gesture.Swipe.MinVelocity", 750f);
+		controller.Config.Save();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Frame frame = controller.Frame();
+		GestureList gestures = frame.Gestures ();
+
+		for(int i=0;i<gestures.Count;i++) {
+			Gesture g = gestures [i];
+			if (g.Type == Gesture.GestureType.TYPE_SWIPE) {
+				SwipeGesture s = new SwipeGesture (g);
+				Vector swipeDirection = s.Direction;
+				if (swipeDirection.x < 0) {
+					Debug.Log ("Left");
+
+				}else if(swipeDirection.x > 0) {
+					Debug.Log("Right");
+				}
+			}
+
+		}
+	}
+
+}
---
+++ Assets/Other/Scripts/ToolPemukul.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class ToolPemukul : MonoBehaviour {
+
+	Controller controller;
+	Tool tool;
+	int i;
+
+	// Use this for initialization
+	void Start () {
+		controller = new Controller();
+		i = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Frame frame = controller.Frame ();
+		tool = frame.Tools.Frontmost;
+
+		if (tool != Tool.Invalid) {
+			Debug.Log ("ADA"+i);
+		} else {
+			Debug.Log("Nope");
+		}
+		i++;
+	}
+}
